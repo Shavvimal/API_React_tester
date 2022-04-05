600692ab79366e110258c3830365d9d39a2ed3e2
Shavvimal <77899493+Shavvimal@users.noreply.github.com>
2022-04-05 16:56:53 +0100

    first 2 done

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,13 +5,24 @@ import { createClient } from '@supabase/supabase-js'
 import JSONPretty from 'react-json-pretty';
 import 'react-json-pretty/themes/monikai.css';
 
-const supabaseUrl = 'https://xjpcntvnzjibfebgiczg.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhqcGNudHZuemppYmZlYmdpY3pnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDg3MTQ4MjUsImV4cCI6MTk2NDI5MDgyNX0.-PjM0_qTNNW1P5vSsqCPAwEeO-R9e9PjZCUAzRm7yWg'
-const supabaseAnon = createClient(supabaseUrl, supabaseKey)
-const supabaseSigned = createClient(supabaseUrl, supabaseKey)
 
 const Home = () => {
+    
+    const supabaseUrl = 'https://xjpcntvnzjibfebgiczg.supabase.co'
+    
+    const supabaseKey = process.env.API_ANON
+    const supabaseKeyService = process.env.API_SERVICE
+
+    const AccessTokenShav = process.env.API_SHAVHUGAN
+    const AccessTokenShavG = process.env.API_SHAVGRAPHICS
+    
+    const supabase = createClient(supabaseUrl, supabaseKey)
+    const supabaseServ = createClient(supabaseUrl, supabaseKeyService)
+    
+    const { user, error } = supabase.auth.setAuth(AccessTokenShav)
+    
     const [output, setOutput] = useState()
+
     var JSONPrettyMon = require('react-json-pretty/dist/monikai');
 
     const clear = () => {
@@ -19,10 +30,33 @@ const Home = () => {
     }
 
     const API1 = async () => {
-        let { data: profile, error } = await supabaseAnon
+        let { data: profile, error } = await supabase
         .from('profile')
+        .select('*').eq('email', 'shavhugan@gmail.com')
+
+        // fetch actual email from AsyncStorage
+
+        let { data: private_profile, error2 } = await supabase
+        .from('private_profile')
         .select('*')
-        setOutput(JSON.stringify(profile))
+
+        setOutput(`[${JSON.stringify(profile).replace('[', '').replace(']', '')}, ${JSON.stringify(private_profile).replace('[', '').replace(']', '')}]`)
+    }
+
+    const API2 = async () => {
+
+        const { data, error } = await supabase
+        .from('private_profile')
+        .update({ country_id: 2,
+                date_of_birth: '11-27-1998'
+                                         })
+        .match({ user_id: '1cd5b79a-ba8d-4788-b7eb-d73954885cae' })
+
+        console.log(error)
+    }
+
+    const log = () => {
+        console.log(supabaseKeyShavGraphics)
     }
 
     return (
@@ -31,11 +65,10 @@ const Home = () => {
         <h1 className="mt-6 text-white font-bold text-3xl"> React-Supabase API Tester</h1>
             <div className="grid grid-cols-6 gap-4 my-4">
             <Button text="API 1" onClickFun={API1}/>
-            <Button text="API 2" onClickFun={API1}/>
+            <Button text="API 2" onClickFun={API2}/>
             <Button text="API 3" onClickFun={API1}/>
             <Button text="API 4" onClickFun={API1}/>
             <Button text="API 5" onClickFun={API1}/>
-            <Button text="API 6" onClickFun={API1}/>
             <Button text="API 7" onClickFun={API1}/>
             <Button text="API 8" onClickFun={API1}/>
             <Button text="API 9" onClickFun={API1}/>
@@ -59,6 +92,7 @@ const Home = () => {
             <Button text="API 27" onClickFun={API1}/>
             <Button text="API 28" onClickFun={API1}/>
             <Button text="API 29" onClickFun={API1}/>
+            <Button text="log" onClickFun={log}/>
             <Button text="Clear" onClickFun={clear}/>
             
             </div>
