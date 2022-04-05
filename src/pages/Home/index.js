5ba9cdbbd6065120e5543726269056d6f4ba27d9
Shavvimal <77899493+Shavvimal@users.noreply.github.com>
2022-04-05 14:01:03 +0100

    pretty JSON

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -7,36 +7,34 @@ import 'react-json-pretty/themes/monikai.css';
 
 const supabaseUrl = 'https://xjpcntvnzjibfebgiczg.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhqcGNudHZuemppYmZlYmdpY3pnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDg3MTQ4MjUsImV4cCI6MTk2NDI5MDgyNX0.-PjM0_qTNNW1P5vSsqCPAwEeO-R9e9PjZCUAzRm7yWg'
-const supabase = createClient(supabaseUrl, supabaseKey)
+const supabaseAnon = createClient(supabaseUrl, supabaseKey)
+const supabaseSigned = createClient(supabaseUrl, supabaseKey)
 
 const Home = () => {
-
-    
     const [output, setOutput] = useState()
+    var JSONPrettyMon = require('react-json-pretty/dist/monikai');
 
-
-    const pressed = async () => {
-        let { data: profile, error } = await supabase
+    const API1 = async () => {
+        let { data: profile, error } = await supabaseAnon
         .from('profile')
         .select('*')
         setOutput(JSON.stringify(profile))
     }
 
-    var JSONPrettyMon = require('react-json-pretty/dist/monikai');
-
-
     return (
-        <><div className="grid place-items-center">
-            <div className="flex flex-row">
+        <>
+        <div className="grid place-items-center">
+        <h1 className="mt-6 text-white font-bold text-3xl"> React-Supabase API Tester</h1>
+            <div className="flex flex-row my-4">
 
-            <button onClick={pressed} className="mt-12 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-2 rounded-full"> 
-            Button
+            <button onClick={API1} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 mx-2 rounded-full"> 
+            1
             </button>
 
-            <button onClick={pressed} className="mt-12 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-2 rounded-full"> 
-            Button
+            <button onClick={API1} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 mx-2 rounded-full"> 
+            2
             </button>
-            
+
             </div>
             <JSONPretty id="json-pretty" data={output} theme={JSONPrettyMon}></JSONPretty>
             </div>
